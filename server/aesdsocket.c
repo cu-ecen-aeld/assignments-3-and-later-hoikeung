7a64e0102839d32c5c9173069465591e97289063
keung <keung@KEUNG.myguest.virtualbox.org>
2026-03-15 11:49:32 +0900

    Assignment 5 part 1

---
+++ server/aesdsocket.c
@@ -0,0 +1,166 @@
+#include "aesdsocket.h"
+
+static int socket_fd;
+int status;
+struct addrinfo hints;
+struct addrinfo *servinfo;
+
+void exit_prog(int e)
+{
+    freeaddrinfo(servinfo);
+    closelog();
+    exit(e);
+}
+
+void signal_handler(int signal) 
+{
+    if (signal == SIGINT || signal == SIGTERM) 
+    {
+        syslog(LOG_NOTICE, "Caught signal, exiting.");
+        int removed = remove(FILE_NAME);
+        if (removed == 0)
+        {
+            printf("Removed success\r\n");
+        }
+        else
+        {
+            printf("Removed fail\r\n");
+        }
+        shutdown(socket_fd, SHUT_RDWR);
+    }
+    printf("Exit\r\n");
+    exit_prog(0);
+}
+
+ssize_t write_file(const char* file_name, const char* str)
+{
+    FILE *file = fopen(file_name, "a+");
+
+    if (file == NULL)
+    {
+        printf("Cannot open write file, Error: %s", strerror(errno));
+        fclose(file);
+        return 1;
+    }
+    else
+    {
+        ssize_t n = fwrite(str, strlen(str), 1, file);
+        if (n < 0)
+        {
+            printf("Cannot write to file, Error: %s", strerror(errno));
+            fclose(file);
+            return 1;
+        }
+        else
+        {
+            printf("Write file success, words = %s", str);
+            fclose(file);
+            return n;
+        }
+    }
+}
+
+ssize_t read_file(const char* file_name, char* buffer, int length)
+{
+    FILE *file = fopen(file_name, "r");
+    ssize_t read_size = fread(buffer, 1, length, file);
+    fclose(file);
+    return read_size;
+}
+
+int main(int argc, char  *argv[])
+{
+    openlog("aesdsocket.c log", 0, LOG_USER);
+
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+
+    // int status;
+    // struct addrinfo hints;
+    // struct addrinfo *servinfo;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    status = getaddrinfo(NULL, PORT, &hints, &servinfo);
+    if (status != 0)
+    {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit_prog(1);
+    }
+
+    socket_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if (socket_fd == -1)
+    {
+        fprintf(stderr, "socket error: %s\n", gai_strerror(status));
+        exit_prog(1);
+    }
+
+    if (bind(socket_fd, servinfo->ai_addr, servinfo->ai_addrlen) != 0)
+    {
+        fprintf(stderr, "bind error: %s\n", gai_strerror(status));
+        exit_prog(1);
+    }
+
+    int daemon_mode = 0;
+    int opt;
+    while ((opt = getopt(argc, argv, "d")) != -1) 
+    {
+        if (opt == 'd') daemon_mode = 1;
+    }
+    if (daemon_mode) 
+    {
+        if (daemon_mode) 
+        {
+            if ((daemon(0, 0)) == -1) 
+            {
+                fprintf(stderr, "Failed to run in daemon mode.\n");
+                exit_prog(1);
+            }
+        }
+    }
+
+    if (listen(socket_fd, 1) != 0)
+    {
+        fprintf(stderr, "listen error: %s\n", gai_strerror(status));
+        exit_prog(1);
+    }
+
+    while (1)
+    {
+        int connection_fd = -1;
+        struct sockaddr_in connection; 
+        socklen_t len = -1; 
+        len = sizeof(connection);
+        connection_fd = accept(socket_fd, (struct sockaddr *)&connection, &len);
+        if (connection_fd == -1)
+        {
+            fprintf(stderr, "accepy error: %s\n", gai_strerror(status));
+            exit_prog(1);
+        }
+        else
+        {
+            syslog(LOG_NOTICE, "Accepted connection from %s, fd = %d", inet_ntoa(connection.sin_addr), connection_fd);
+
+            char recv_buf[MAX_BUFFER_LEN] = {0};
+            ssize_t buf_len = recv(connection_fd, recv_buf, MAX_BUFFER_LEN, MSG_DONTWAIT); 
+            if (buf_len < 0)
+            {
+                fprintf(stderr, "receive error: %s\n", gai_strerror(status));
+            }
+            else if (recv_buf[buf_len-1] == '\n')
+            {
+                write_file(FILE_NAME, recv_buf);
+                char send_buf[MAX_BUFFER_LEN] = {0};
+                ssize_t read_length = read_file(FILE_NAME, send_buf, MAX_BUFFER_LEN);
+                // printf("Read %d words\r\n%s", (int) read_length, send_buf);
+                send(connection_fd, send_buf, read_length, MSG_DONTWAIT);
+                syslog(LOG_NOTICE, "Closed connection from from %s, fd = %d", inet_ntoa(connection.sin_addr), connection_fd);
+            }
+        }
+    }
+
+    exit_prog(1);
+}
