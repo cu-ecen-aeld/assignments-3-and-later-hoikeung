67dfed05905de5b22bf98b3ad7872e93a590f0f9
keung <keung@KEUNG.myguest.virtualbox.org>
2026-03-18 22:15:40 +0900

    a

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,4 +1,19 @@
-#include "aesdsocket.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <errno.h>
+#include <signal.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define MAX_BUFFER_LEN  102400
+#define PORT            "9000"
+#define FILE_NAME       "/var/tmp/aesdsocketdata"
 
 static int socket_fd;
 int status;
