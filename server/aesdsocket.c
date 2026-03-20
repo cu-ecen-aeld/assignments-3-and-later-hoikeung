0f63e30ba89dc6a88681e5d7d220c38e6140582d
keung <keung@KEUNG.myguest.virtualbox.org>
2026-03-20 17:48:47 +0900

    Assignment 6 part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -5,30 +5,35 @@ int status;
 struct addrinfo hints;
 struct addrinfo *servinfo;
 
-void exit_prog(int e)
+static pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER;
+static SLIST_HEAD(thread_list_head, thread_entry) thread_list;
+
+static void exit_prog(int e)
 {
     freeaddrinfo(servinfo);
+    close(socket_fd);
     closelog();
     exit(e);
 }
 
-void signal_handler(int signal) 
+static void signal_handler(int signal) 
 {
     if (signal == SIGINT || signal == SIGTERM) 
     {
         syslog(LOG_NOTICE, "Caught signal, exiting.");
-        int removed = remove(FILE_NAME);
-        if (removed == 0)
-        {
-            printf("Removed success\r\n");
-        }
-        else
-        {
-            printf("Removed fail\r\n");
-        }
+        remove(FILE_NAME);
+        // int removed = remove(FILE_NAME);
+        // if (removed == 0)
+        // {
+        //     printf("Removed success\n");
+        // }
+        // else
+        // {
+        //     printf("Removed fail\n");
+        // }
         shutdown(socket_fd, SHUT_RDWR);
     }
-    printf("Exit\r\n");
+    // printf("Exit\n");
     exit_prog(0);
 }
 
@@ -39,7 +44,8 @@ ssize_t write_file(const char* file_name, const char* str)
 
     if (file == NULL)
     {
-        fprintf(stderr, "Cannot open write file, Error: %s\r\n", strerror(errno));
+        fprintf(stderr, "Cannot open write file, Error: %s\n", strerror(errno));
+        fprintf(stderr, RED "Cannot open write file, Error: %s\n" RESET, gai_strerror(status));
         fclose(file);
         exit_prog(1);
     }
@@ -48,7 +54,7 @@ ssize_t write_file(const char* file_name, const char* str)
         n = fwrite(str, strlen(str), 1, file);
         if (n < 0)
         {
-            fprintf(stderr, "Cannot write to file, Error: %s\r\n", strerror(errno));
+            fprintf(stderr, RED "Cannot write to file, Error: %s\n" RESET, gai_strerror(status));
             fclose(file);
             exit_prog(1);
         }
@@ -68,16 +74,69 @@ ssize_t read_file(const char* file_name, char* buffer, int length)
     return read_size;
 }
 
+static void *timer_thread(void *arg)
+{
+    struct timespec req;
+    req.tv_sec = TIMESTAMP_INTERVAL;
+    req.tv_nsec = 0;
+
+    while (1)
+    {
+        nanosleep(&req, NULL);
+
+        time_t now = time(NULL);
+        struct tm *tm_p = localtime(&now);
+        char buf[128] = {0};
+        strftime(buf, sizeof(buf), "%a, %d %b %Y %H:%M:%S %z", tm_p);
+        buf[strlen(buf)] = '\n';
+
+        char timestmp[256] = "timestamp:";
+        strcat(timestmp, buf);
+
+        pthread_mutex_lock(&file_mutex);
+        write_file(FILE_NAME, timestmp);
+        pthread_mutex_unlock(&file_mutex);
+    }
+
+    return NULL;
+}
+
+static void *connection_thread(void *arg)
+{
+    struct thread_entry *entry = (struct thread_entry *) arg;
+
+    syslog(LOG_NOTICE, "Accepted connection from %s", entry->ip);
+
+    char recv_buf[MAX_BUFFER_LEN] = {0};
+    ssize_t buf_len = recv(entry->connection_fd, recv_buf, MAX_BUFFER_LEN, MSG_DONTWAIT); 
+    if (buf_len < 0)
+    {
+        fprintf(stderr, RED "Receive error: %s\n" RESET, gai_strerror(status));
+        exit_prog(1);
+    }
+    else if (recv_buf[buf_len-1] == '\n')
+    {
+        pthread_mutex_lock(&file_mutex);
+        write_file(FILE_NAME, recv_buf);
+        char send_buf[MAX_BUFFER_LEN] = {0};
+        ssize_t read_length = read_file(FILE_NAME, send_buf, MAX_BUFFER_LEN);
+        send(entry->connection_fd, send_buf, read_length, MSG_DONTWAIT);
+        pthread_mutex_unlock(&file_mutex);
+        syslog(LOG_NOTICE, "Closed connection from from %s", entry->ip);
+        entry->done = 1;
+    }
+
+    return NULL;
+}
+
 int main(int argc, char  *argv[])
 {
     openlog("aesdsocket.c log", 0, LOG_USER);
 
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
-
-    // int status;
-    // struct addrinfo hints;
-    // struct addrinfo *servinfo;
+    
+    SLIST_INIT(&thread_list);
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_flags = AI_PASSIVE;
@@ -87,20 +146,20 @@ int main(int argc, char  *argv[])
     status = getaddrinfo(NULL, PORT, &hints, &servinfo);
     if (status != 0)
     {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        fprintf(stderr, RED "getaddrinfo error: %s\n" RESET, gai_strerror(status));
         exit_prog(1);
     }
 
     socket_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
     if (socket_fd == -1)
     {
-        fprintf(stderr, "socket error: %s\n", gai_strerror(status));
+        
         exit_prog(1);
     }
 
     if (bind(socket_fd, servinfo->ai_addr, servinfo->ai_addrlen) != 0)
     {
-        fprintf(stderr, "bind error: %s\n", gai_strerror(status));
+        fprintf(stderr, RED "bind error: %s\n" RESET, gai_strerror(status));
         exit_prog(1);
     }
 
@@ -116,15 +175,23 @@ int main(int argc, char  *argv[])
         {
             if ((daemon(0, 0)) == -1) 
             {
-                fprintf(stderr, "Failed to run in daemon mode.\n");
+                fprintf(stderr, RED "Failed to run in daemon mode.\n" RESET);
                 exit_prog(1);
             }
         }
     }
 
-    if (listen(socket_fd, 1) != 0)
+    if (listen(socket_fd, 10) != 0)
     {
-        fprintf(stderr, "listen error: %s\n", gai_strerror(status));
+        fprintf(stderr, RED "listen error: %s\n" RESET, gai_strerror(status));
+        exit_prog(1);
+    }
+
+    pthread_t timer_tid;
+    if (pthread_create(&timer_tid, NULL, timer_thread, NULL) != 0) 
+    {
+        syslog(LOG_ERR, "timer_thread create error: %s", strerror(errno));
+        fprintf(stderr, RED "timer_thread create error: %s\n" RESET, gai_strerror(status));
         exit_prog(1);
     }
 
@@ -137,30 +204,42 @@ int main(int argc, char  *argv[])
         connection_fd = accept(socket_fd, (struct sockaddr *)&connection, &len);
         if (connection_fd == -1)
         {
-            fprintf(stderr, "accepy error: %s\n", gai_strerror(status));
+            fprintf(stderr, RED "accept error: %s\n" RESET, gai_strerror(status));
             exit_prog(1);
         }
         else
         {
-            syslog(LOG_NOTICE, "Accepted connection from %s, fd = %d", inet_ntoa(connection.sin_addr), connection_fd);
+            // syslog(LOG_NOTICE, "Accepted connection from %s, fd = %d", inet_ntoa(connection.sin_addr), connection_fd);
 
-            char recv_buf[MAX_BUFFER_LEN] = {0};
-            ssize_t buf_len = recv(connection_fd, recv_buf, MAX_BUFFER_LEN, MSG_DONTWAIT); 
-            if (buf_len < 0)
+            struct thread_entry *entry = malloc(sizeof(struct thread_entry));
+            entry->connection_fd = connection_fd;
+            entry->done = 0;
+            inet_ntop(AF_INET, &connection.sin_addr, entry->ip, sizeof(entry->ip));
+
+            if (pthread_create(&entry->thread_id, NULL, connection_thread, entry) != 0) 
             {
-                fprintf(stderr, "receive error: %s\n", gai_strerror(status));
+                syslog(LOG_ERR, "connection_thread create error: %s", strerror(errno));
+                fprintf(stderr, RED "Fconnection_thread create error: %s\n" RESET, gai_strerror(status));
+                close(connection_fd);
+                free(entry);
+                exit_prog(1);
             }
-            else if (recv_buf[buf_len-1] == '\n')
+            SLIST_INSERT_HEAD(&thread_list, entry, entries);
+
+            struct thread_entry *e;
+            SLIST_FOREACH(e, &thread_list, entries)
             {
-                write_file(FILE_NAME, recv_buf);
-                char send_buf[MAX_BUFFER_LEN] = {0};
-                ssize_t read_length = read_file(FILE_NAME, send_buf, MAX_BUFFER_LEN);
-                // printf("Read %d words\r\n%s", (int) read_length, send_buf);
-                send(connection_fd, send_buf, read_length, MSG_DONTWAIT);
-                syslog(LOG_NOTICE, "Closed connection from from %s, fd = %d", inet_ntoa(connection.sin_addr), connection_fd);
+                if (e->done)
+                {
+                    pthread_join(e->thread_id, NULL);
+                    SLIST_REMOVE(&thread_list, e, thread_entry, entries);
+                    free(e);
+                }
             }
+            
         }
     }
 
+    fprintf(stderr, RED "Final: %s\n" RESET, gai_strerror(status));
     exit_prog(1);
 }
--- server/aesdsocket.h
+++ server/aesdsocket.h
@@ -6,10 +6,27 @@
 #include <signal.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/queue.h>
 #include <arpa/inet.h>
 
 #define MAX_BUFFER_LEN  50000
 #define PORT            "9000"
 #define FILE_NAME       "/var/tmp/aesdsocketdata"
+#define TIMESTAMP_INTERVAL 10
+
+//Colour code for debug
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+
+struct thread_entry 
+{
+    pthread_t            thread_id;
+    int                  connection_fd;
+    char                 ip[INET_ADDRSTRLEN];
+    volatile int         done;
+    SLIST_ENTRY(thread_entry) entries;
+};
